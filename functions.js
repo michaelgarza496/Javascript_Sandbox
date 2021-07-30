c2aedfc5b7a7762ea7c83646356abe1ba724082b
Michael Garza <michaelgarza496@gmail.com>
2021-07-30 00:00:18 -0400

    Added isPrime function.

--- functions.js
+++ functions.js
@@ -41,3 +41,25 @@ store.add(ninja);
 console.log(store.cache);
 store.add(ninja);
 console.log(store.cache);
+
+
+function isPrime(value) {
+    if (!isPrime.answers) {
+        isPrime.answers = {}; // create the cache
+    }
+
+    // check for cached values
+    if (isPrime.answers[value] !== undefined) {
+        return isPrime.answers[value];
+    }
+
+    let prime = value !== 1; // 1 is not a prime
+
+    for (let i = 2; i < value; i++) {
+        if (value % i === 0) {
+            prime = false;
+            break;
+        }
+    }
+    return isPrime.answers[value] = prime;
+}
